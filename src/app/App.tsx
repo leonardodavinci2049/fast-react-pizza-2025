75725050c3aa936a33fe5759b9a404d3f629899c
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-03-31 09:50:39 -0300

    start project

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="fex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1>Welcome to My App</h1>
+      <p>This is a simple React application.</p>
+      <p>It uses TypeScript and Vite for development.</p>
+    </div>
+  );
+}
+
+export default App;
